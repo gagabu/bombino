0cc21467b8b82402d2b1c6b825380e866123a2f9
Anatolii Rabchevskyi <booluk@gmail.com>
2019-06-07 17:08:02 +0300

    add webhooks

--- Telegram/TelegramBotUpdatesMiddleware.cs
+++ Telegram/TelegramBotUpdatesMiddleware.cs
@@ -1,20 +1,47 @@
-﻿using Microsoft.AspNetCore.Http;
+﻿using System.IO;
 using System.Threading.Tasks;
+using BombinoBomberBot.Handlers;
+using Google.Apis.Logging;
+using MediatR;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Logging;
+using Newtonsoft.Json;
+using Telegram.Bot.Types;
 
-namespace BombinoBomberBot
+namespace BombinoBomberBot.Telegram
 {
     internal class TelegramBotUpdatesMiddleware
     {
+        private readonly IMediator _mediator;
+        private readonly ILogger<TelegramBotUpdatesMiddleware> _logger;
         private readonly RequestDelegate _next;
 
-        public TelegramBotUpdatesMiddleware(RequestDelegate next)
+        private static readonly JsonSerializer Serializer = new JsonSerializer();
+
+        public TelegramBotUpdatesMiddleware(IMediator mediator, ILogger<TelegramBotUpdatesMiddleware> logger, RequestDelegate next)
         {
+            _mediator = mediator;
+            _logger = logger;
             _next = next;
         }
 
         public async Task InvokeAsync(HttpContext context)
         {
-            await _next(context);
+            var update = Serializer.Deserialize<Update>(new JsonTextReader(new StreamReader(context.Request.Body)));
+            if (update != null)
+            {
+                await _mediator.Send(new GenericUpdateRequest(update.Message));
+            }
+            else
+            {
+                using (_logger.BeginScope(new
+                                       {
+                                            context.Connection.RemoteIpAddress,
+                                       }))
+                {
+                    _logger.LogWarning("Someone came here");
+                }
+            }
         }
     }
 }
